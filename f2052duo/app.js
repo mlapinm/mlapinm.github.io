fe6592dc855b8dcacc9aacf8957c800d329998f0
mlapinm <mlapin@rambler.ru>
2021-05-22 18:42:26 +0300

    3

--- f2052duo/app.js
+++ f2052duo/app.js
@@ -1,16 +1,21 @@
 let log
 let tests = []
 let round = null
+let isShowAnswer = false
 
 function openTest(pNameJson){
   $.getJSON(pNameJson, function(data) {
     data.forEach(element => {
       tests.push(element)
     });
-  }).then(()=>{
+  }).then(() => {
 
     class Round{
       wordsIndexes = []
+      firstWord = ''
+      isShowAnswer = false
+      sentenceWords = []
+      isCheckOk = false
 
       constructor(){
         this.squareRu = $('.square-ru')
@@ -26,9 +31,17 @@ function openTest(pNameJson){
         this.enWords = this.test.en.split(' ')
       }
 
+      setIsShowAnswer(isShowAnswer1){
+        this.isShowAnswer = isShowAnswer1
+      }
+
       show(){
+        this.clear()
+        this.sentenceWords = []
         this.squareRu.text(this.test.ru)
-        this.firstWordDisplay.text(this.enWords.shift())
+        this.firstWord = this.enWords.shift()
+        this.firstWordDisplay.text(this.firstWord)
+        this.sentenceWords.push(this.firstWord)
 
 
         for(let i=0;i<this.enWords.length;i++){
@@ -51,8 +64,50 @@ function openTest(pNameJson){
           button.addEventListener('click', this.onButtonClick)
           this.buttonsDisplay.append(button)
         }
+        this.showAnswer(this.isShowAnswer)
 
-        this.squareAnswer.text(this.test.en)
+      }
+
+      showAnswer(isShowAnswer1){
+        if(isShowAnswer1){
+          this.squareAnswer.text(this.test.en)
+          isShowAnswer = true
+          $('.btn-enable').addClass('active1')
+        }else{
+          this.squareAnswer.text('')
+          isShowAnswer = false
+          $('.btn-enable').removeClass('active1')
+        }
+      }
+
+      clear(){
+        this.firstWord = ''
+        this.buttonsDisplay.children().remove()
+        this.squareSentence.children().remove()
+        this.wordsIndexes = []
+        log.text('')
+      }
+
+      testing(){
+        // let text = $('.square-all-sentence').text()
+        let textInput = '2' 
+        //textInput = this.sentenceWords.join(' ')
+        textInput = $('.first-word').text()
+        let ar = $('.square-sentence').contents()
+        let k=' '
+        ar.each((i,v)=>{
+          textInput += ' ' + v.innerText
+        })
+
+        let textTest = this.firstWord + ' ' + this.enWords.join(' ')
+        if(textInput == textTest){
+          this.isCheckOk = true
+          log.css('backgroundColor', 'white')
+        }else{
+          this.isCheckOk = false
+          log.css('backgroundColor', 'lightgray')
+        }
+        log.text(this.isCheckOk)
       }
 
       onButtonClick(e){
@@ -71,6 +126,8 @@ function openTest(pNameJson){
 
         round.squareSentence.append(word)
         button.style.color = 'lightgray'
+
+        round.sentenceWords.push(buttonText)
         // log.text(button.innerText)
       }
 
@@ -82,20 +139,85 @@ function openTest(pNameJson){
         let buttonId = 'b' + wordId.substring(1)
         let button = document.querySelector('#' + buttonId)
 
-        button.style.color = 'gray'
+        button.style.color = 'black'
         $('#'+wordId).remove()
 
       }
     }
 
     round = new Round()
-    round.setTest(tests[1])
+    round.setTest(tests[0])
     round.show()
 
+    let number = 0
+
+    $('.btn-next').click(()=>{
+      if(number<tests.length-1){
+        number++
+      }else{
+        number = 0
+      }
+      round.setTest(tests[number])
+      round.show()
+      $('.number').text(number+1)
+    })
+
+    $('.btn-previous').click(()=>{
+      if(number > 0){
+        number--
+      }else{
+        number = tests.length-1
+      }
+      round.setTest(tests[number])
+      round.show()
+      $('.number').text(number+1)
+    })
+
+
+
+    $('.btn-enable').click(()=>{
+      if(isShowAnswer == false){
+        round.showAnswer(true)
+        isShowAnswer = true
+        $('.btn-enable').addClass("active1")
+      }else{
+        round.showAnswer(false)
+        isShowAnswer = false
+        $('.btn-enable').removeClass("active1")
+      }
+    })
+
+    $('.btn-always').click(()=>{
+      if(round.isShowAnswer == false){
+        round.setIsShowAnswer(true)
+        $('.btn-always').addClass("active1")
+
+        round.showAnswer(true)
+        isShowAnswer = true
+        $('.btn-enable').addClass("active1")
+
+
+      }else{
+        round.setIsShowAnswer(false)
+        $('.btn-always').removeClass('active1')
+
+        round.showAnswer(false)
+        isShowAnswer = false
+        $('.btn-enable').removeClass("active1")
+
+
+      }
+    })
+
+    $('.btn-check1').click(()=>{
+      round.testing()
+    })
 
       // log.text(tests[1].ru)
   })
 }
+
+
 
 
 
