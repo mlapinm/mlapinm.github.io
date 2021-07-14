5e51ec50141d23c6c26dc3dd6c5d068086f99a17
Mikhail Lapin <mlapin@rambler.ru>
2021-07-14 10:44:18 +0300

    Create b02j.js

---
+++ f2112cartoon/b02j.js
@@ -0,0 +1,26 @@
+var jords = [
+  {
+    "time": [
+      4,
+      8
+    ],
+    "en": "RUMPELSTILTSKIN",
+    "ru": "Румпельстильцкин"
+  },
+  {
+    "time": [
+      8,
+      10
+    ],
+    "en": "In a far away land there was a beautiful",
+    "ru": "В далекой стране было прекрасное"
+  },
+  {
+    "time": [
+      10,
+      15
+    ],
+    "en": "wood by which ran a stream and close to the stream was a mill",
+    "ru": "В лесу протекал ручей, а рядом с ручьем стояла мельница"
+  }
+]
