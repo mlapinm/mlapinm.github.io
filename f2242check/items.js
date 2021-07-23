dd72acde514d9899b7cbdbf5b46545fb23885cd9
Mikhail Lapin <mlapin@rambler.ru>
2021-07-23 13:14:00 +0300

    topics

---
+++ f2242check/items.js
@@ -0,0 +1,51 @@
+var items = [
+{
+	"level" : "1",
+	"topic" : [
+	"t1"
+	]
+},
+{
+	"level" : "1",
+	"topic" : [
+	"t2"
+	]
+},
+{
+	"level" : "1",
+	"topic" : [
+	"t3"
+	]
+},
+{
+	"level" : "1",
+	"topic" : [
+	"t4"
+	]
+},
+{
+	"level" : "1",
+	"topic" : [
+	"t5"
+	]
+},
+{
+	"level" : "1",
+	"topic" : [
+	"t6"
+	]
+},
+{
+	"level" : "1",
+	"topic" : [
+	"t7"
+	]
+},
+{
+	"level" : "1",
+	"topic" : [
+	"t8"
+	]
+}
+
+]
