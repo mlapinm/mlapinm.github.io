03eababa8d793922274a767034017d0c790bc6dc
mlapinm <mlapin@rambler.ru>
2021-07-25 17:42:34 +0300

    2

--- f2162mus/d03items.js
+++ f2162mus/d03items.js
@@ -1,100 +1,587 @@
 var objs = [
     {
       "time": [
-        0,
-        40
+0.67,
+15.13
       ],
       "en": [
-        "",
         ""
       ],
       "ru": [
-        "",
         ""
       ]
     },
     {
       "time": [
-        0,
-        0
+15.13,
+19.77 
       ],
       "en": [
-        "",
+        "Now I've heard There was a secret chord"
+    ],
+      "ru": [
+        "Теперь я слышал, Что Был секретный аккорд"
+      ]
+    },
+    {
+      "time": [
+19.77,
+24.4 
+      ],
+      "en": [
+        "That David played And it pleased the Lord"
+    ],
+      "ru": [
+        "Что Давид играл, И это было угодно Господу"
+      ]
+    },
+    {
+      "time": [
+24.4,
+30.2 
+      ],
+      "en": [
+        "But you don't really care For music, do you? ",
         ""
     ],
       "ru": [
-        "",
+        "Но ты ведь на самом деле не любишь музыку, не так ли?"
+      ]
+    },
+    {
+      "time": [
+33.27,
+37.8
+      ],
+      "en": [
+        "It goes like this The fourth, the fifth"
+    ],
+      "ru": [
+        "Это звучит так: Четвертый, пятый"
+      ]
+    },
+    {
+      "time": [
+37.8,
+42.03
+      ],
+      "en": [
+        "The minor fall The major lift"
+    ],
+      "ru": [
+        "Незначительное падение, большой подъем"
+      ]
+    },
+    {
+      "time": [
+42.03,
+47.67 
+      ],
+      "en": [
+        "The baffled king Composing Hallelujah"
+    ],
+      "ru": [
+        "Сбитый с толку король, Сочиняющий Аллилуйю"
+      ]
+    },
+    {
+      "time": [
+49.63,
+73.3
+      ],
+      "en": [
+        "Hallelujah Hallelujah",
+        "Hallelujah Hallelujah"
+    ],
+      "ru": [
+        "Аллилуйя, Аллилуйя",
+        "Аллилуйя, Аллилуйя"
+      ]
+    },
+    {
+      "time": [
+73.3,
+77.7
+      ],
+      "en": [
+        "Your faith was strong But you needed proof"
+    ],
+      "ru": [
+        "Твоя вера была сильной, Но тебе нужны были доказательства"
+      ]
+    },
+    {
+      "time": [
+77.7,
+82.37 
+      ],
+      "en": [
+        "You saw her bathing On the roof"
+    ],
+      "ru": [
+        "Вы видели, как она купалась на крыше"
+      ]
+    },
+    {
+      "time": [
+82.37,
+88.5 
+      ],
+      "en": [
+        "Her beauty and the moonlight Overthrew you"
+    ],
+      "ru": [
+        "Ее красота и лунный свет Свергли тебя"
+      ]
+    },
+    {
+      "time": [
+90.57,
+95.17 
+      ],
+      "en": [
+        "She tied you To a kitchen chair"
+    ],
+      "ru": [
+        "Она привязала тебя к кухонному стулу"
+      ]
+    },
+    {
+      "time": [
+95.17,
+99.4
+      ],
+      "en": [
+        "She broke your throne And she cut your hair"
+    ],
+      "ru": [
+        "Она сломала твой трон И подстригла твои волосы "
+      ]
+    },
+    {
+      "time": [
+99.4,
+105.5 
+      ],
+      "en": [
+        "And from your lips She drew the Hallelujah "
+    ],
+      "ru": [
+        "И с твоих губ Она сорвала Аллилуйю."
+      ]
+    },
+    {
+      "time": [
+107.33,
+129.87
+      ],
+      "en": [
+        "Hallelujah Hallelujah",
+        "Hallelujah Hallelujah"
+    ],
+      "ru": [
         ""
       ]
     },
     {
       "time": [
-        0,
-        0
+129.87,
+133.87 
       ],
       "en": [
-        "",
-        ""
+        "Now maybe there's A God above"
     ],
       "ru": [
-        "",
+        "Теперь, может быть, наверху есть Бог"
+      ]
+    },
+    {
+      "time": [
+133.87,
+138.57 
+      ],
+      "en": [
+        "As for me, all I ever Learned from love"
+    ],
+      "ru": [
+        "Что касается меня, то все, чему я когда-либо учился у любви "
+      ]
+    },
+    {
+      "time": [
+138.57,
+147.13
+      ],
+      "en": [
+        "Is how to shoot at someone Who outdrew you"
+    ],
+      "ru": [
+        "Это как стрелять в того, кто тебя опередил"
+      ]
+    },
+    {
+      "time": [
+147.13,
+151.0 
+      ],
+      "en": [
+        "But it's not a crime You hear tonight"
+    ],
+      "ru": [
+        "Но это не преступление, которое Вы услышите сегодня вечером"
+      ]
+    },
+    {
+      "time": [
+151.0,
+155.23
+      ],
+      "en": [
+        "And it's not some pilgrim who Claims to have seen the light"
+    ],
+      "ru": [
+        "И это не какой-то пилигрим, который утверждает, что видел свет"
+      ]
+    },
+    {
+      "time": [
+155.23,
+162.13 
+      ],
+      "en": [
+        "it's a cold and it's A very broken Hallelujah"
+    ],
+      "ru": [
+        "это холодно, и это очень разбитое Аллилуйя."
+      ]
+    },
+    {
+      "time": [
+163.9,
+207.27
+      ],
+      "en": [
+        "Hallelujah Hallelujah",
+        "Hallelujah Hallelujah"
+    ],
+      "ru": [
         ""
       ]
     },
     {
       "time": [
-        0,
-        0
+207.27,
+210.87
       ],
       "en": [
-        "",
         ""
     ],
       "ru": [
-        "",
         ""
       ]
     },
     {
       "time": [
-        0,
-        0
+224.6,
+242.73
       ],
       "en": [
-        "",
-        ""
+        "Hallelujah Hallelujah",
+        "Hallelujah Hallelujah"
     ],
       "ru": [
-        "",
         ""
       ]
     },
     {
       "time": [
-        0,
-        0
+242.73,
+246.93 
       ],
       "en": [
-        "",
-        ""
+        "Oh, people I've been here before"
     ],
       "ru": [
-        "",
+        "О, люди, я был здесь раньше"
+      ]
+    },
+    {
+      "time": [
+246.93,
+251.5 
+      ],
+      "en": [
+        "I know this roo And I've walked this floor"
+    ],
+      "ru": [
+        "Я знаю этого ру, И я ходил по этому этажу"
+      ]
+    },
+    {
+      "time": [
+251.5,
+257.4 
+      ],
+      "en": [
+        "You see, I used to live alone Before I knew you"
+    ],
+      "ru": [
+        "Видишь ли, я жила одна До того, как узнала тебя"
+      ]
+    },
+    {
+      "time": [
+259.83,
+264.3 
+      ],
+      "en": [
+        "And I've seen your flag On the marble arch"
+    ],
+      "ru": [
+        "И я видел твой флаг На мраморной арке"
+      ]
+    },
+    {
+      "time": [
+264.3,
+268.23 
+      ],
+      "en": [
+        "But listen, love is not Some kind of victory march"
+    ],
+      "ru": [
+        "Но послушай, любовь-это не какой-то победный марш"
+      ]
+    },
+    {
+      "time": [
+268.23,
+275.17 
+      ],
+      "en": [
+        "No, it's a cold and It's a very lonely Hallelujah"
+    ],
+      "ru": [
+        "Нет, это холодно и очень одиноко, Аллилуйя."
+      ]
+    },
+    {
+      "time": [
+276.67,
+299.3
+      ],
+      "en": [
+        "Hallelujah Hallelujah",
+        "Hallelujah Hallelujah"
+    ],
+      "ru": [
         ""
       ]
     },
     {
-    "time": [
-      0,
-      0
+      "time": [
+299.3,
+303.7
+      ],
+      "en": [
+        "There was a time You let me know"
     ],
-    "en": [
-      "",
-	    ""
-	],
-    "ru": [
-      "",
-      ""
-    ]
-  }
+      "ru": [
+        "Было время, когда Ты дал мне знать"
+      ]
+    },
+    {
+      "time": [
+303.7,
+307.8 
+      ],
+      "en": [
+        "What's really going on Below"
+    ],
+      "ru": [
+        "Что на самом деле происходит Внизу"
+      ]
+    },
+    {
+      "time": [
+307.8,
+313.5 
+      ],
+      "en": [
+        "But now you never Show it to me, do you?"
+    ],
+      "ru": [
+        "Но теперь ты никогда не Показываешь его мне, не так ли?"
+      ]
+    },
+    {
+      "time": [
+316.33,
+321.03
+      ],
+      "en": [
+        "I remember when I moved in you"
+    ],
+      "ru": [
+        "Я помню, как я переехал в тебя"
+      ]
+    },
+    {
+      "time": [
+321.03,
+325.23
+      ],
+      "en": [
+        "And the Holy Dove She was moving too"
+    ],
+      "ru": [
+        "И Святая Голубка, Она тоже двигалась"
+      ]
+    },
+    {
+      "time": [
+325.23,
+332.17 
+      ],
+      "en": [
+        "And every single breath that We drew was Hallelujah"
+    ],
+      "ru": [
+        "И каждый Наш вдох был Аллилуйей."
+      ]
+    },
+    {
+      "time": [
+332.77,
+354.9
+      ],
+      "en": [
+        "Hallelujah Hallelujah",
+        "Hallelujah Hallelujah"
+    ],
+      "ru": [
+        ""
+      ]
+    },
+    {
+      "time": [
+354.9,
+359.4 
+      ],
+      "en": [
+        "I've done my best I know it wasn't much"
+    ],
+      "ru": [
+        "Я сделал все, что мог, я знаю, что это было не так уж много"
+      ]
+    },
+    {
+      "time": [
+359.4,
+363.67 
+      ],
+      "en": [
+        "I couldn't feel So I learned to touch"
+    ],
+      "ru": [
+        "Я не мог чувствовать, Поэтому я научился прикасаться"
+      ]
+    },
+    {
+      "time": [
+363.67,
+367.5 
+      ],
+      "en": [
+        "I've told the truth I didn't Come here to London"
+    ],
+      "ru": [
+        "Я сказал правду, что приехал сюда не в Лондон"
+      ]
+    },
+    {
+      "time": [
+367.5,
+369.43
+      ],
+      "en": [
+        "Just to fool you"
+    ],
+      "ru": [
+        "Просто чтобы одурачить тебя"
+      ]
+    },
+    {
+      "time": [
+369.43,
+376.87
+      ],
+      "en": [
+        "And even though It all went wrong"
+    ],
+      "ru": [
+        "И даже несмотря на то, что все пошло не так"
+      ]
+    },
+    {
+      "time": [
+376.87,
+380.9
+      ],
+      "en": [
+        "I'll stand right here Before the lord of song"
+    ],
+      "ru": [
+        "Я буду стоять прямо здесь Перед повелителем песни"
+      ]
+    },
+    {
+      "time": [
+380.9,
+384.13 
+      ],
+      "en": [
+        "With nothing Nothing on my tongue"
+    ],
+      "ru": [
+        "У меня ничего, Ничего на языке."
+      ]
+    },
+    {
+      "time": [
+384.13,
+387.17
+      ],
+      "en": [
+        "But hallelujah"
+    ],
+      "ru": [
+        ""
+      ]
+    },
+    {
+      "time": [
+388.83,
+428.33
+      ],
+      "en": [
+        "Hallelujah Hallelujah",
+        "Hallelujah Hallelujah"
+    ],
+      "ru": [
+        ""
+      ]
+    },
+    {
+      "time": [
+428.33,
+432.27
+      ],
+      "en": [
+        ""
+    ],
+      "ru": [
+        ""
+      ]
+    }
 ]
