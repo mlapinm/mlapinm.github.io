cea23ad33507ab02a6ad9c4d3ed387245e634f5a
Mikhail Lapin <mlapin@rambler.ru>
2021-09-28 15:11:40 +0300

    k76

--- f2182blog/a02notes.js
+++ f2182blog/a02notes.js
@@ -1073,6 +1073,690 @@ var notes = [
     "header": "CCNA",
     "title": "",
     "card-text": [
+      "k01",
+      "FREE CCNA Lab 001: Basic Router Security Configuration 1"
+    ],
+    "link": "k01pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k02",
+      "FREE CCNA Lab 002: Basic Router Security Configuration 2"
+    ],
+    "link": "k02pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k03",
+      "FREE CCNA Lab 003: Basic Router Security Configuration 3"
+    ],
+    "link": "k03pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k04",
+      "FREE CCNA Lab 004: Basic Serial Connection Configuration"
+    ],
+    "link": "k04pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k05",
+      "FREE CCNA Lab 005: VLAN Configuration"
+    ],
+    "link": "k05pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k06",
+      "FREE CCNA Lab 006: VLAN Configuration: Trunk Encapsulation"
+    ],
+    "link": "k06pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k07",
+      "FREE CCNA Lab 007: Inter-VLAN Routing (Router on a Stick)"
+    ],
+    "link": "k07pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k08",
+      "FREE CCNA Lab 008: Inter-VLAN Routing (Router on a Stick) - Troubleshooting 1"
+    ],
+    "link": "k08pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k09",
+      "FREE CCNA Lab 009: Inter-VLAN Routing (Router on a Stick) - Troubleshooting 2"
+    ],
+    "link": "k09pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k10",
+      "FREE CCNA Lab 010: Local Username/Password Database"
+    ],
+    "link": "k10pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k11",
+      "FREE CCNA Lab 011_ 'Message of the Day' and Login Banners"
+    ],
+    "link": "k11pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k12",
+      "FREE CCNA Lab 012_ Naming VLANs"
+    ],
+    "link": "k12pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k13",
+      "FREE CCNA Lab 013_ CDP (Cisco Discovery Protocol) Part 1"
+    ],
+    "link": "k13pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k14",
+      "FREE CCNA Lab 014_ CDP (Cisco Discovery Protocol) Part 2"
+    ],
+    "link": "k14pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k15",
+      "FREE CCNA Lab 015_ System Configuration Dialog"
+    ],
+    "link": "k15pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k16",
+      "FREE CCNA Lab 016_ Loopback Interface Configuration"
+    ],
+    "link": "k16pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k17",
+      "FREE CCNA Lab 017_ Port Security (Part 1)"
+    ],
+    "link": "k17pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k18",
+      "FREE CCNA Lab 018_ Port Security (Part 2)"
+    ],
+    "link": "k18pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k19",
+      "FREE CCNA Lab 019_ Port Security (Part 3)"
+    ],
+    "link": "k19pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k20",
+      "FREE CCNA Lab 020_ Review Configuration Lab 1"
+    ],
+    "link": "k20pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k21",
+      "FREE CCNA Lab 021_ Review Troubleshooting Lab 1"
+    ],
+    "link": "k21pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k22",
+      "FREE CCNA Lab 022_ Telnet _ VTY lines"
+    ],
+    "link": "k22pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k23",
+      "FREE CCNA Lab 023_ SSH _ VTY lines"
+    ],
+    "link": "k23pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k24",
+      "FREE CCNA Lab 024_ Static Routing (Part 1)"
+    ],
+    "link": "k24pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k25",
+      "FREE CCNA Lab 025_ Static Routing (Part 2)"
+    ],
+    "link": "k25pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k26",
+      "FREE CCNA Lab 026_ Static Routing (Part 3)"
+    ],
+    "link": "k26pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k27",
+      "FREE CCNA Lab 027_ RIP (Part 1)"
+    ],
+    "link": "k27pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k28",
+      "FREE CCNA Lab 028_ RIP (Part 2)"
+    ],
+    "link": "k28pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k29",
+      "FREE CCNA Lab 029_ Standard ACLs (Access Control Lists Part 1)"
+    ],
+    "link": "k29pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k30",
+      "FREE CCNA Lab 030_ Extended ACLs (Access Control Lists Part 2)"
+    ],
+    "link": "k30pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k31",
+      "FREE CCNA Lab 031_ Named ACLs (Access Control Lists Part 3)"
+    ],
+    "link": "k31pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k32",
+      "FREE CCNA Lab 032_ NTP (Network Time Protocol Part 1)"
+    ],
+    "link": "k32pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k33",
+      "FREE CCNA Lab 033_ NTP (Network Time Protocol Part 2)"
+    ],
+    "link": "k33pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k34",
+      "FREE CCNA Lab 034_ LLDP (Link Layer Discovery Protocol)"
+    ],
+    "link": "k34pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k35",
+      "FREE CCNA Lab 035_ DHCP (Dynamic Host Configuration Protocol)"
+    ],
+    "link": "k35pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k36",
+      "FREE CCNA Lab 036_ DNS (Domain Name System)"
+    ],
+    "link": "k36pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k37",
+      "FREE CCNA Lab 037_ Static NAT (Network Address Translation)"
+    ],
+    "link": "k37pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k38",
+      "FREE CCNA Lab 038_ Dynamic NAT (Network Address Translation)"
+    ],
+    "link": "k38pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k39",
+      "FREE CCNA Lab 039_ PAT (Port Address Translation)"
+    ],
+    "link": "k39pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k40",
+      "FREE CCNA Lab 040_ Syslog"
+    ],
+    "link": "k40pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k41",
+      "FREE CCNA Lab 041_ Password recovery, configuration backup, IOS upgrade"
+    ],
+    "link": "k41pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k42",
+      "FREE CCNA Lab 042_ IPv6"
+    ],
+    "link": "k42pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k43",
+      "FREE CCNA Lab 043_ Review Configuration Lab 2"
+    ],
+    "link": "k43pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k44",
+      "FREE CCNA Lab 044_ Review Troubleshooting Lab 2"
+    ],
+    "link": "k44pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k45",
+      "FREE CCNA Lab 045_ DTP _ VLANs"
+    ],
+    "link": "k45pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k46",
+      "FREE CCNA Lab 046_ VTP (VLAN Trunking Protocol)"
+    ],
+    "link": "k46pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k47",
+      "FREE CCNA Lab 047_ VTP _ VLAN Troubleshooting"
+    ],
+    "link": "k47pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k48",
+      "FREE CCNA Lab 048_ STP (Spanning Tree Protocol)"
+    ],
+    "link": "k48pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k49",
+      "FREE CCNA Lab 049_ STP Election (Spanning Tree Protocol)"
+    ],
+    "link": "k49pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k50",
+      "FREE CCNA Lab 050_ EtherChannel"
+    ],
+    "link": "k50pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k51",
+      "FREE CCNA Lab 051_ EtherChannel Troubleshooting"
+    ],
+    "link": "k51pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k52",
+      "FREE CCNA Lab 052_ InterVLAN Routing (SVI, Router on a Stick)"
+    ],
+    "link": "k52pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k53",
+      "FREE CCNA Lab 053_ InterVLAN Routing Troubleshooting"
+    ],
+    "link": "k53pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k54",
+      "FREE CCNA Lab 054_ OSPF (Part 1_ Single Area)"
+    ],
+    "link": "k54pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k55",
+      "FREE CCNA Lab 055_ OSPF (Part 2_ Multi-Area)"
+    ],
+    "link": "k55pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k56",
+      "FREE CCNA Lab 056_ OSPF Troubleshooting"
+    ],
+    "link": "k56pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k57",
+      "FREE CCNA Lab 057_ OSPFv3 for IPv6"
+    ],
+    "link": "k57pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k58",
+      "FREE CCNA Lab 058_ EIGRP"
+    ],
+    "link": "k58pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k59",
+      "FREE CCNA Lab 059_ EIGRP Troubleshooting"
+    ],
+    "link": "k59pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k60",
+      "FREE CCNA Lab 060_ EIGRP for IPv6"
+    ],
+    "link": "k60pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k61",
+      "FREE CCNA Lab 061_ PPP with PAP and CHAP Authentication"
+    ],
+    "link": "k61pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k62",
+      "FREE CCNA Lab 062_ PPP Troubleshooting"
+    ],
+    "link": "k62pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k63",
+      "FREE CCNA Lab 063_ MultiLink PPP"
+    ],
+    "link": "k63pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k64",
+      "FREE CCNA Lab 064_ PPPoE"
+    ],
+    "link": "k64pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k65",
+      "FREE CCNA Lab 065_ GRE"
+    ],
+    "link": "k65pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k66",
+      "FREE CCNA Lab 066_ GRE Troubleshooting"
+    ],
+    "link": "k66pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k67",
+      "FREE CCNA Lab 067_ BGP"
+    ],
+    "link": "k67pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k68",
+      "FREE CCNA Lab 068_ BGP Troubleshooting"
+    ],
+    "link": "k68pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k69",
+      "FREE CCNA Lab 069_ HSRP"
+    ],
+    "link": "k69pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k70",
+      "FREE CCNA Lab 070_ HSRP Troubleshooting"
+    ],
+    "link": "k70pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k71",
+      "FREE CCNA Lab 071_ IPv4 and IPv6 ACLs"
+    ],
+    "link": "k71pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k72",
+      "FREE CCNA Lab 072_ ACL Troubleshooting"
+    ],
+    "link": "k72pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k73",
+      "FREE CCNA Lab 073_ SPAN"
+    ],
+    "link": "k73pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k74",
+      "FREE CCNA Lab 074_ IP SLA"
+    ],
+    "link": "k74pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k75",
+      "FREE CCNA Lab 075_ Review Configuration Lab 3"
+    ],
+    "link": "k75pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k76",
+      "FREE CCNA Lab 076_ Review Troubleshooting Lab 3"
+    ],
+    "link": "k76pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
       "j00",
       "",
       ""
