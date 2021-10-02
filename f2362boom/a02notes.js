a6e6d86d16fd649c9246325e8e6409276dbb9a5b
mlapinm <mlapin@rambler.ru>
2021-10-02 10:58:14 +0300

    10

---
+++ f2362boom/a02notes.js
@@ -0,0 +1,33 @@
+var notes = [
+  {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "boom",
+      "",
+      ""
+    ],
+    "link": "d00boom.html"
+   },
+  {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": "d00boom.html"
+   },
+  {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": "d00boom.html"
+   },
+
+]
