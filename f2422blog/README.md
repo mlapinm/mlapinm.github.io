f3ef31d0f69dab8a5f40fabb215ed0fc6a568edd
Mikhail Lapin <mlapin@rambler.ru>
2021-10-08 11:03:19 +0300

    36

---
+++ f2422blog/README.md
@@ -0,0 +1,6 @@
+
+
+
+https://www.youtube.com/watch?v=8f011wdiW7g&list=PLqazFFzUAPc5lOQwDoZ4Dw2YSXtO7lWNv
+
+
---
+++ f2422blog/a02notes.js
@@ -0,0 +1,56 @@
+var notes = [
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k01",
+      "Inkscape Lesson 1 - Interface and Basic Drawing"
+    ],
+    "link": "k01pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k02",
+      "FREE CCNA Lab 002: Basic Router Security Configuration 2"
+    ],
+    "link": "k02pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k03",
+      "FREE CCNA Lab 003: Basic Router Security Configuration 3"
+    ],
+    "link": "k03pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k04",
+      "FREE CCNA Lab 004: Basic Serial Connection Configuration"
+    ],
+    "link": "k04pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k05",
+      "FREE CCNA Lab 005: VLAN Configuration"
+    ],
+    "link": "k05pl.html"
+  },
+  {
+    "header": "CCNA",
+    "title": "",
+    "card-text": [
+      "k06",
+      "FREE CCNA Lab 006: VLAN Configuration: Trunk Encapsulation"
+    ],
+    "link": "k06pl.html"
+  }
+]
