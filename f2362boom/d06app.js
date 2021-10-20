a32255c4b54c8952a333b3cb7aba1ff4d08f95f4
Mikhail Lapin <mlapin@rambler.ru>
2021-10-20 16:28:21 +0300

    3

--- f2362boom/d06app.js
+++ f2362boom/d06app.js
@@ -6,7 +6,34 @@ $(()=>{
   let log1 = $('.log1')
   let log2 = $('.log2')
 
-  log1.text(items[0].name)
+  let item = items[7]
+
+  function showItem(item){
+    let div = $('<div></div>')
+    let p1 = $('<p></p>')
+    p1.text(item.nums)
+    let p2 = $('<p></p>')
+    p2.text(item.name)
+    div.append(p1)
+    div.append(p2)
+    $('.desk').append(div)
+  }
+
+  $('input').val(12)
+
+  $('button').click(() => {
+    item = items[10]
+    showItem(item)
+
+    let value = $('input').val()
+    log2.text(value)
+  })
+
+  showItem(item)
+
+
+
+  // log1.text(items[0].name)
   log2.text()
 
   
