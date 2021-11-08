ae0bcc21506a9c2a3f0f68b6d73c24df6f2b768d
mlapinm <mlapin@rambler.ru>
2021-11-08 13:37:08 +0300

    6

---
+++ f2522fcc/r03links.js
@@ -0,0 +1,152 @@
+let links = [
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},  
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},  
+{
+n : "React",
+ls : [
+  [
+  "Learn React: Create a Simple JSX Element | freeCodeCamp.org",
+  "https://www.freecodecamp.org/learn/front-end-development-libraries/react/create-a-simple-jsx-element",
+  ""
+  ],
+  [
+  "Learn the Basics · React Native",
+  "https://reactnative.dev/docs/tutorial#hello-world",
+  ""
+  ],
+  [
+  "ToDo App with React Native. React Native is a framework for… | by Ahmed Mahmoud | codeburst",
+  "https://codeburst.io/todo-app-with-react-native-f889e97e398e",
+  ""
+  ],
+  [
+  "Как делают мобильные приложения на JavaScript для начинающих - YouTube",
+  "https://www.youtube.com/watch?v=5nWu_P259FI",
+  "Web Developer Blog"
+  ],
+  [
+  "Using ASYNC STORAGE w/React Native & Expo - YouTube",
+  "https://www.youtube.com/watch?v=PhhyBmAIehg",
+  ""
+  ],
+  [
+  "The Complete React Native Course 2021 : from Zero to Hero - YouTube",
+  "https://www.youtube.com/watch?v=ANdSdIlgsEw&t=6s",
+  ""
+  ],
+  [
+  "Деплой React-приложения на Vercel, Netlify, Heroku, GitHub Pages, Surge - YouTube",
+  "https://www.youtube.com/watch?v=-pJN9faoa8E",
+  ""
+  ],
+  [
+  "Preact. Альтернативный React без сборки из одного файла. - YouTube",
+  "https://www.youtube.com/watch?v=dpgD4GerAhs",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},  
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},  
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},  
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},  
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+}  
+]
---
+++ f2522fcc/r05links.js
@@ -0,0 +1,52 @@
+let links = [
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},  
+{
+n : "",
+ls : [
+  [
+  "",
+  "",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},
+{
+n : "MongoDB and Mongoose",
+ls : [
+  [
+  "Create a Model - MongoDB and Mongoose - Free Code Camp - YouTube",
+  "https://www.youtube.com/watch?v=BnAaBDShXDM&list=PL3vpzVxKa3Pi0vrxXLM1VYSZNSxT4yDAq&index=2",
+  ""
+  ],
+  [
+  "Node.js Crash Course Tutorial #1 - Introduction & Setup - YouTube",
+  "https://www.youtube.com/watch?v=zb3Qk8SG5Ms&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=1",
+  ""
+  ],
+  [
+  "",
+  "",
+  ""
+  ]
+]
+},
+]
