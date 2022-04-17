042b082decb58b1ce9e66b0e51f98f9ee64933e2
mlapinm <mlapin@rambler.ru>
2022-04-17 23:12:20 +0300

    73

--- f2424cours/p01print.py
+++ f2424cours/p01print.py
@@ -39,7 +39,7 @@ def files_from_list(l):
         name = match[1] if match else 'qqq'  
         match = re.search(r'q\d* (.*)$', text)
         sentence = match[1] if match else 'qqq'  
-        names += [[name, sentence + '.']]
+        names += [[name, sentence]]
 
     for e in names:
         with open('{}e.txt'.format(e[0]), 'w', encoding='utf-8') as f:
@@ -58,7 +58,45 @@ def files_from_list(l):
     print(text)
     pass
 
-l = ['q207 Using data to drive successful outcomes', 'q208 Real-world data magic', 'q303 Learning about data phases and tools', 'q304 Stages of the data life cycle', 'q305 Six phases of data analysis', 'q306 Molly: Example of the data process', 'q307 Exploring data analyst tools', 'q403 The ins and outs of core data tools', 'q404 Columns and rows and cells, oh my', 'q405 SQL in action', 'q406 Angie: Everyday struggles when learning new skills', 'q407 Becoming a data viz whiz', 'q408 Lilah: The power of a visualization', "q503 Let's get down to business", 'q504 The job of a data analyst', 'q505 Joey: Path to becoming a data analyst', 'q506 Tony: Supporting careers in data analytics', 'q507 The power of data in business', 'q508 Rachel: Data detectives', 'q509 Understanding data and fairness', 'q510 Alex: Fair and ethical data decisions', 'q511 Data analysts in different industries', 'q512 Samah: Interview best practices', 'q513 Weekly wrap-up']
+l = [
+    'q12101 Introduction to problem-solving and effective questioning.',
+    'q12102 Data in action.',
+    'q12103 Nikki: The data process works.',
+    'q12104 Common problem types.',
+    'q12105 Problems in the real world.',
+    'q12106 Anmol: From hypothesis to outcome.',
+    'q12107 SMART questions.',
+    'q12108 Evan: Data opens doors.',
+    'q12201 Data and decisions.',
+    'q12202 How data empowers decisions.',
+    'q12203 Qualitative and quantitative data.',
+    'q12204 The big reveal: Sharing your findings.',
+    'q12205 Data versus metrics.',
+    'q12206 Mathematical thinking.',
+    'q12301 The amazing spreadsheet.',
+    'q12302 Get to work with spreadsheets.',
+    'q12303 Step-by-step in spreadsheets.',
+    'q12304 Formulas for success.',
+    'q12305 Spreadsheet errors and fixes.',
+    'q12306 Functions 101.',
+    'q12307 Before solving a problem, understand it.',
+    'q12308 Scope of work and structured thinking.',
+    'q12308 Scope of work and structured thinking.',
+    'q12309 Staying objective.',
+    'q12401 Communicating with your team.',
+    'q12402 Balancing needs and expectations across your team.',
+    'q12403 Focus on what matters.',
+    'q12404 Clear communication is key.',
+    'q12405 Tips for effective communication.',
+    'q12406 Balancing expectations and realistic project goals.',
+    'q12407 Sarah: How to communicate with stakeholders.',
+    'q12408 The data tradeoff: Speed versus accuracy.',
+    'q12409 Think about your process and outcome.',
+    'q12410 Meeting best practices.',
+    'q12411 Ximena: Joining a new team.',
+    'q12412 From conflict to collaboration.',
+    'q12413 Nathan: From the U.S. Marine Corps to data analytics.',
+    ]
 
 def make_list(name):
     '''
@@ -66,7 +104,8 @@ def make_list(name):
     write to file "q11*" from "q*"
     '''
 
-    name2 = re.sub(r'^q','q11', name)
+    # name2 = re.sub(r'^q','q11', name)
+    name2 = name
 
     text = ''
     with open(name, 'r', encoding='utf-8') as f:
@@ -170,11 +209,13 @@ def make_script1():
 
 if __name__ == "__main__":
 
+    # files_from_list(l)
+
     # make_lists(l)
     # make_js()
 
-    make_script1()
+    # make_script1()
 
 
-    print(123)
+    print(len(l))
 
