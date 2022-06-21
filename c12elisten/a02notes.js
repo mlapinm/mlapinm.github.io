39551046d98b7419c3b54b521a711badad82297c
Mikhail Lapin <mlapin@rambler.ru>
2022-06-21 10:36:18 +0300

    Listen and practice

---
+++ c12elisten/a02notes.js
@@ -0,0 +1,142 @@
+var notes = [
+  {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "Listen and practice 1",
+      "",
+      ""
+    ],
+    "link": "d11index.html"
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "Listen and practice ",
+      "",
+      ""
+    ],
+    "link": "d12index.html"
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "Listen and practice ",
+      "",
+      ""
+    ],
+    "link": "d13index.html"
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "Listen and practice ",
+      ""
+    ],
+    "link": "d14index.html"
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   },
+   {
+    "header": "",
+    "title": "ml",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": "d22index.html"
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "Django for evrybody",
+      "d41index.html",
+      ""
+    ],
+    "link": "d41index.html"
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   },
+   {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   },
+  {
+    "header": "",
+    "title": "",
+    "card-text": [
+      "",
+      "",
+      ""
+    ],
+    "link": ""
+   }
+]
