4e1e9816489798bf3b1f4304260ec8bb4d0c9005
Mikhail Lapin <mlapin@rambler.ru>
2022-09-07 16:06:23 +0300

    d3246

---
+++ c12elisten/js/d32646e.js
@@ -0,0 +1,13 @@
+var d32646e = [
+  "<a href=\"https://www.youtube.com/watch?v=N5OC-qGMs0c&list=PLhuEelKSHsFnfUaBGjFjkCChFmg8L2Ls-&index=116\">Yankee Doodle went to town A-riding on a pony</a>",
+  "Yankee Doodle went to town A-riding on a pony.",
+  "He stuck a feather in his hat And called it macaroni.",
+  "Chorus: Yankee Doodle, keep it up.",
+  "Yankee Doodle dandy Mind the music and the step.",
+  "and with the girls be handy!.",
+  "Father and I went down to camp Along with Captain Gooding.",
+  "And there we saw the men and boys As thick as hasty pudding.",
+  "Chorus And there was Captain Washington.",
+  "And gentle folks about him They say he's grown so tarnal proud.",
+  "He will not ride without them."
+]
