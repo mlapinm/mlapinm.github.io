00b5493e75ab360d4d777ea7927749cb50ab3333
Mikhail Lapin <mlapin@rambler.ru>
2022-12-28 11:23:11 +0300

    Update b24clocka.py

--- f2362boom/b25car/b24clocka.py
+++ f2362boom/b25car/b24clocka.py
@@ -1,6 +1,8 @@
 
 
 
+def of_s(s):
+    return s ** (1/2)
 
 
 
@@ -21,3 +23,8 @@ if __name__ == "__main__":
         rs += [to_hundred(e)]
 
     print(rs)
+
+    rs2 = [10 * of_s(to_hundred(60 - e)) for e in range(0, 61 ,10)]
+    print(rs2, 0.19**(1/2))
+
+
