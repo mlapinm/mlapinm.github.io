267d11f09362d3e2d18feffe56d0add27393d043
Mikhail Lapin <mlapin@rambler.ru>
2023-02-10 15:22:09 +0300

    a01all.cmd

---
+++ c16wiki/a01all - копия.cmd
@@ -0,0 +1,9 @@
+echo %0
+
+SET FNAME=D:\mailCloud\prjother\b04py\b006enru\b02mdhtml.py
+if NOT EXIST %FNAME% (
+    SET FNAME=D:\mailCloud\cloud\prjother\b04py\b006enru\b02mdhtml.py
+    ECHO Yes
+)
+ECHO %FNAME%
+python %FNAME% all
--- c16wiki/a01all.cmd
+++ c16wiki/a01all.cmd
@@ -1,9 +1,13 @@
 echo %0
-
-SET FNAME=D:\mailCloud\prjother\b04py\b006enru\b02mdhtml.py
-if NOT EXIST %FNAME% (
-    SET FNAME=D:\mailCloud\cloud\prjother\b04py\b006enru\b02mdhtml.py
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+SET FPATH=D:\mailCloud\prjother\b04py\b006enru\
+if NOT EXIST %FPATH% (
+    SET FPATH=D:\mailCloud\cloud\prjother\b04py\b006enru\
     ECHO Yes
 )
-ECHO %FNAME%
-python %FNAME% all
+
+ECHO %FPATH%b02mdhtml.py
+@REM 
+python %FPATH%b03schedule.py b2650schedule.md i265001time.png
+python %FPATH%b02mdhtml.py all
+b2650schedule.html
