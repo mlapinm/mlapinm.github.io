334407651549d91de5e4e2108d4a5fd2ebc7226a
Mikhail Lapin <mlapin@rambler.ru>
2023-02-28 17:06:27 +0300

    march

---
+++ c16wiki/a01all - копия (2).cmd
@@ -0,0 +1,15 @@
+echo %0
+call C:\ProgramData\Anaconda3\Scripts\activate.bat
+call C:/anaconda3/Scripts/activate.bat
+SET FPATH=D:\mailCloud\prjother\b04py\b006enru\
+if NOT EXIST %FPATH% (
+    SET FPATH=D:\mailCloud\cloud\prjother\b04py\b006enru\
+    ECHO Yes
+)
+
+ECHO %FPATH%b02mdhtml.py
+@REM 
+python %FPATH%b03schedule.py b2650schedule.md i265001time.png
+python %FPATH%b02mdhtml.py all
+b2650schedule.html
+pause
--- c16wiki/a01all.cmd
+++ c16wiki/a01all.cmd
@@ -1,15 +1,11 @@
 echo %0
 call C:\ProgramData\Anaconda3\Scripts\activate.bat
 call C:/anaconda3/Scripts/activate.bat
-SET FPATH=D:\mailCloud\prjother\b04py\b006enru\
-if NOT EXIST %FPATH% (
-    SET FPATH=D:\mailCloud\cloud\prjother\b04py\b006enru\
-    ECHO Yes
-)
+SET FPATH=O:\prjother\b04py\b006enru\
 
 ECHO %FPATH%b02mdhtml.py
 @REM 
-python %FPATH%b03schedule.py b2650schedule.md i265001time.png
+python %FPATH%b03schedule.py b2650schedule.md i265001time.png 2 3
 python %FPATH%b02mdhtml.py all
 b2650schedule.html
 pause
