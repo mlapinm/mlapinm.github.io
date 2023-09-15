7b44f69d9c65ca6807484ee579e88384fb783515
Mikhail Lapin <mlapin@rambler.ru>
2023-09-15 16:55:04 +0300

    b01root

--- c16wiki/a01schedule.cmd
+++ c16wiki/a01schedule.cmd
@@ -3,8 +3,6 @@ call C:\ProgramData\Anaconda3\Scripts\activate.bat
 call C:/anaconda3/Scripts/activate.bat
 SET FPATH=O:\prjother\b04py\b006enru\
 
-ECHO %FPATH%b02mdhtml.py
-@REM 
 python %FPATH%b03schedule.py b2650schedule.md i265001time.png 2 9
 python %FPATH%b02mdhtml.py all
 b2650schedule.html
